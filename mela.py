63b0411a87a93335305ea1c9960fc4d0a2a6949c
Mohit Sinha <msinha132@gmail.com>
2018-06-28 13:03:00 +0530

    first running model

--- mela.py
+++ mela.py
@@ -2,21 +2,24 @@ import pandas as pd
 import numpy as np
 
 class MelaClassifier():
-	def __init__(self):
-		pass
 
-	def probsOf(feat, target, low, up):
-	    data = pd.DataFrame()
-	    data = train.groupby(by=feat)[target].mean()
-	    for i in data:
-	        if i > low and i < up:
-	            i = 0.5
-	    return data
+	def __init__(self):
+		self.var = None
+		self.weights = None
+		self.train = None
+
+	def probsOf(self, feat, target, train, low, up):
+		data = pd.DataFrame()
+		data = train.groupby(by=feat)[target].mean()
+		for i in data:
+			if i > low and i < up:
+				i = 0.5
+		return data
 
 	def fit(self, train_x, train_y):
 		self.var = train_x.columns
-		self.weights = np.ones(var.size)
-		train[target] = train_y	
+		self.weights = np.ones(self.var.size)
+		train = pd.concat([train_x, train_y], axis=1)	
 		self.train = train
 
 		return self
@@ -25,7 +28,7 @@ class MelaClassifier():
 
 		X_test = test_x[self.var].copy()
 		for feat in self.var:		
-	    	X_test[feat] = test[feat].map(probsOf(feat, target, self.train)) #self.train.groupby(by=feat)['is_pass'].mean()
+			X_test[feat] = test_x[feat].map(self.probsOf(feat, target, self.train, 0.45, 0.85)) #self.train.groupby(by=feat)['is_pass'].mean()
 		X_test.fillna(0.5, inplace=True)
 
 		pred = np.zeros(X_test.shape[0])
