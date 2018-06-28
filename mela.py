cbc662db6a53d4eac3debc2b49436e7a946e0e14
Mohit Sinha <msinha132@gmail.com>
2018-06-28 12:13:31 +0530

    structured

--- mela.py
+++ mela.py
@@ -1,33 +1,39 @@
 import pandas as pd
 import numpy as np
 
-def probsOf(feat, target):
-    data = pd.DataFrame()
-    data = train.groupby(by=feat)[target].mean()
-    for i in data:
-        if i > 0.4 and i < 0.85:
-            i = 0.5
-    return data
+class MelaClassifier():
+	def __init__(self):
+		pass
 
-def train(train, test, target):
-	var = ['trainee_id', 'test_id', 'difficulty_level', 'trainee_engagement_rating', 'total_programs_enrolled', 'age']
-	weights = [1, 1.7, 1.4, 1.1, 1, 1]
-	X_train = train[var].copy()
-	X_test = test[var].copy()
+	def probsOf(feat, target, low, up):
+	    data = pd.DataFrame()
+	    data = train.groupby(by=feat)[target].mean()
+	    for i in data:
+	        if i > low and i < up:
+	            i = 0.5
+	    return data
 
-	for feat in X_train.columns:
-		X_train[feat] = train[feat].map(probsOf(feat, target)) #train.groupby(by=feat)['is_pass'].mean()
-    	X_test[feat] = test[feat].map(probsOf(feat, target)) #train.groupby(by=feat)['is_pass'].mean()
-	X_test.fillna(0.5, inplace=True)
+	def fit(self, train_x, train_y):
+		self.var = train_x.columns
+		self.weights = np.ones(var.size)
+		train[target] = train_y	
+		self.train = train
 
-	pred = np.zeros(X_test.shape[0])
-	for idx,feat in enumerate(X_test.columns):
-		pred += X_test[feat].values*weights[idx]
-	pred/=sum(weights)
-	x = np.random.rand(pred.shape[0])
-	x/=100
-	pred+=x
+		return self
 
-	return pred
+	def predict(self, test_x, target):
 
-sub = train(train, test, 'is_pass')
+		X_test = test_x[self.var].copy()
+		for feat in self.var:		
+	    	X_test[feat] = test[feat].map(probsOf(feat, target, self.train)) #self.train.groupby(by=feat)['is_pass'].mean()
+		X_test.fillna(0.5, inplace=True)
+
+		pred = np.zeros(X_test.shape[0])
+		for idx,feat in enumerate(X_test.columns):
+			pred += X_test[feat].values*self.weights[idx]
+		pred/=sum(self.weights)
+		x = np.random.rand(pred.shape[0])
+		x/=100
+		pred+=x
+
+		return pred
