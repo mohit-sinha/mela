8ec947bcf26bc81483b3eb57b78e7651ed832183
Mohit Sinha <msinha132@gmail.com>
2018-06-28 15:42:24 +0530

    parameter required

--- mela.py
+++ mela.py
@@ -6,7 +6,18 @@ class MelaClassifier():
 	def __init__(self):
 		self.var = None
 		self.weights = None
-		self.train = None
+
+	def preprocess(self, data):
+
+		for i in data.columns:
+			if data[i].isnull().values().any:
+				num = list()
+				x = data[i].value_counts().keys()
+				num.append(x[0])
+				num.append(x[0])
+				num.append(x[1])
+				data[i] = data[i].fillna(np.random.choice(num))
+				
 
 	def probsOf(self, feat, target, train, low, up):
 		data = pd.DataFrame()
@@ -16,19 +27,21 @@ class MelaClassifier():
 				i = 0.5
 		return data
 
-	def fit(self, train_x, train_y):
-		self.var = train_x.columns
-		self.weights = np.ones(self.var.size)
-		train = pd.concat([train_x, train_y], axis=1)	
+	def fit(self, train, target):
 		self.train = train
+		self.target = target
+		train_x = train.drop(target, axis=1)
+		self.var = train_x.columns
+		self.weights = np.ones(self.var.size)		
+		self.train_x = train_x
 
 		return self
 
-	def predict(self, test_x, target):
+	def predict(self, test_x):
 
 		X_test = test_x[self.var].copy()
 		for feat in self.var:		
-			X_test[feat] = test_x[feat].map(self.probsOf(feat, target, self.train, 0.45, 0.85)) #self.train.groupby(by=feat)['is_pass'].mean()
+			X_test[feat] = test_x[feat].map(self.probsOf(feat, self.target, self.train, 0.45, 0.85))
 		X_test.fillna(0.5, inplace=True)
 
 		pred = np.zeros(X_test.shape[0])
